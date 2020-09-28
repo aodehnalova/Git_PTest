03b226e772581edba9a2538747f9d97bc680cde4
git959908 <aodehnalova@spst.cz>
2020-09-28 16:03:45 +0200

    Součet

--- Git_PTest/Form1.cs
+++ Git_PTest/Form1.cs
@@ -16,5 +16,12 @@ namespace Git_PTest
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = Int32.Parse(textBox1.Text);
+            int b = Int32.Parse(textBox2.Text);
+            label2.Text = (a + b).ToString();
+        }
     }
 }
